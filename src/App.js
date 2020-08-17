3ea2cc46e6c374d5991cbbd6ed1078c5c6044286
trudova <liubovtrudova@gmail.com>
2020-08-16 20:08:42 -0400

    more commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import  'bootstrap/dist/css/bootstrap.min.css';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
-
+// why my netlify not working=((()))
 function App() {
   return (
     <div className="container">
@@ -13,9 +13,8 @@ function App() {
         <TodoInput/>
         <TodoList/>
       </div>
-      
     </div>
   );
 }
-
+uuidv4(); 
 export default App;
