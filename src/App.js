a3cfe38a4e281b0bca5d977d0329d6179fad9f71
trudova <liubovtrudova@gmail.com>
2020-08-16 20:58:56 -0400

    set methods and state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react'
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { v4 as uuidv4 } from 'uuid';
 import  'bootstrap/dist/css/bootstrap.min.css';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
+ class App extends Component {
+   state={
+     items:[{id:1,
+    title:'wake up'},
+    {id:2,
+      title:'make breekfast'}
+    ],
+    id:uuidv4(),
+    item:'',
+    editItem:false
+   }
+   handleChange=(e)=>{
+     console.log('hello handleChange')
+   }
 
-// why my netlify not working=((()))
-function App() {
-  return (
-    <div className="container">
+   handleSubmit =(e)=>{
+    console.log('hello handleSubmit')
+   }
+
+   clearList =()=>{
+    console.log('hello clearList')
+   }
+   handleDelete =(id)=>{
+    console.log(`hello delete ${id}`)
+   }
+
+   handleEdit =(id)=>{
+    console.log(`hello edit ${id}`)
+   }
+
+  render() {
+    console.log(this.state)
+    return (
+      <div className="container">
       <div className='row'>
-        <TodoInput/>
-        <TodoList/>
+        <div className='col-10 mx-auto col-md-8 mt-5'>
+          <h2 className='text-capitalize text-centr'>ToDo input</h2>
+          <TodoInput item ={this.state.item} handleChange={this.handleChange} handleSubmit={this.handleSubmit} editItem={this.state.editItem}/>
+        <TodoList items={this.state.items} clearList={this.clearList} handleDelete={this.handleDelete} handleEdit={this.handleEdit}  />
+        </div>
       </div>
     </div>
-  );
+    )
+  }
 }
 uuidv4(); 
 export default App;
+
+
+
